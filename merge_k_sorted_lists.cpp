326740f3db31d4e1c85cd7843a537930baefd0eb
Junhao Hua <huajh7@gmail.com>
2015-11-02 22:23:43 +0800

    merge sort

---
+++ merge_k_sorted_lists.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<limits.h>
+using namespace std;
+
+//Definition for singly-linked list.
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+private: 
+	ListNode* merge(ListNode* a, ListNode* b) // a, b are sorted
+	{	
+		ListNode nhead(0);
+		ListNode* head = &nhead;
+		ListNode* tail = head;
+
+		while (a != NULL || b != NULL)
+		{			
+			if (a == NULL)
+			{
+				tail->next = b;
+				break;
+			}
+			else if (b == NULL)
+			{
+				tail->next = a;
+				break;
+			}
+			else{
+				if (a->val < b->val)
+				{	
+					tail->next = a;					
+					tail = tail->next;
+					a = a->next;				
+				}
+				else{
+					tail->next = b;
+					tail = tail->next;
+					b = b->next;					
+				}
+			}			
+		}
+		return head->next;
+
+	}
+public:
+	ListNode* mergeKLists(vector<ListNode*>& lists)  // 
+	{
+		if (lists.empty())
+		{
+			return NULL;
+		}
+		int k = lists.size();		
+		if (k == 1)
+			return lists[0];
+		if (k == 2)
+		{
+			return merge(lists[0],lists[1]);			
+		}
+		int mid = k / 2;
+		vector<ListNode*> left(lists.begin(),lists.begin() + mid );
+		vector<ListNode*> right(lists.begin() + mid , lists.end());
+		ListNode* a = mergeKLists(left);	
+		ListNode* b = mergeKLists(right);
+		return merge(a, b);		
+	}
+};
+
+void main()
+{
+	Solution sol;
+	ListNode * head  =	NULL;
+	
+	ListNode * head2;
+	ListNode a2(-1), b2(5), c2(11);
+	head2 = &a2;
+	a2.next = &b2;
+	b2.next = &c2;
+
+	ListNode * head0 = NULL;
+	ListNode * head3;
+	ListNode a3(6), b3(9);
+	head3 = &a3;
+	a3.next = &b3;
+
+	vector<ListNode*> lists;
+	lists.push_back(head);
+	lists.push_back(head2);
+	lists.push_back(head0);
+	lists.push_back(head3);
+
+	ListNode* ans = sol.mergeKLists(lists);  // 
+}
+
